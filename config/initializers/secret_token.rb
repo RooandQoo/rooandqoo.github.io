1b907409f93932bf45cce5d2a5502b21a82b100c
rooandqoo <sa2ki.k32@gmail.com>
2016-07-01 02:18:41 +0900

    Railsをやめる

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dotcom::Application.config.secret_token = 'b3299bfe6bbf754223bbb623df27ae3b95dbc88575d51ab076cb0291367a8454685f27ead54e44f661cf3693c1410f8b4febc98cfd8529f504aaa6b94590b80c'
